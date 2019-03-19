e81181b5cc5d8175cef1d4c157304874ae6e6617
Cristina Rivera <clr3@hw.ac.uk>
2019-03-19 01:47:49 +0000

    CharactersCard

--- Assets/Scripts/CharacterTurns.cs
+++ Assets/Scripts/CharacterTurns.cs
@@ -6,18 +6,20 @@ public class CharacterTurns : MonoBehaviour
 {
 
     //Create as many characters as ther will be in the game
+    //if we only have the dwarf the make just one charachter here..?
     private int noOfCharacters = 5;
     [SerializeField] public Unit charater1; 
     [SerializeField] public Unit charater2;
     [SerializeField] public Unit charater3; 
     [SerializeField] public Unit charater4; 
     [SerializeField] public Unit charater5; 
-    public Unit[] characters = new Unit[]();
+    public Unit[] characters;
+    private int counter = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        characters = new Unit[](charater1,charater2,charater3,charater4,charater5);
     }
 
     // Update is called once per frame
@@ -26,17 +28,17 @@ public class CharacterTurns : MonoBehaviour
         
     }
 
-
-    //
-    public void startCharacterTurns(){
-        //Check for the BASE stats to see of the game is still going
-        //Do this loop every time the characters 
-        for(int i = 0; i < noOfCharacters; i++){
-
-        }
+    //Get the next character for moving
+    //Should be used after clicking the next Turn Button
+    public Unit nextCharacter(){
+        if(count = noOfCharacters){
+            count = 0;
+            return characters[count];
+        }else{
+            count++;
+            return characters[count];
+        } 
     }
-
-
 
 
 
